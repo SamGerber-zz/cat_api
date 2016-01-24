b37976629b3273d11a22af5577c60e6cbc1e021e
SamGerber <gerbyxvi@gmail.com>
2016-01-23 21:48:48 -0800

    Introduce forms for new and edit, implement delete
    Create html with embedded ruby forms for the new and edit actions
    for Cat. Implement delete so it destroys a given cat.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -14,13 +14,26 @@ class CatsController < ApplicationController
 
   def create
     # POST /cats
-    cat = Cat.new(params[:cat].permit(:name))
+    @cat = Cat.new(params[:cat].permit(:name))
 
-    if cat.save
-      render json: cat
+    if @cat.save
+      redirect_to cat_url @cat
     else
       render json: cat.errors.full_messages, status: :unprocessable_entity
     end
+  end
+
+  # 1. GET /cats/new to fetch a form
+  # 2. USER fills out form, clicks submit.
+  # 3. POST /cats the data in the form.
+  # 4. Create action is invoked, cat is created
+  # 5. Send client a redirect to /cats/:id
+  # 6. Client makes a GET request for /cats/:id
+  # 7. Show action for newly created cat is invoked.
+
+  def new
+    # /cats/new
+    # show a form to create a new object
   end
 
   def update
@@ -29,10 +42,22 @@ class CatsController < ApplicationController
     cat.update(params[:cat].permit(:name))
   end
 
+  def edit
+    # /cats/:id/edit
+    # show a form to edit a given object
+  end
+
   def destroy
-    # if !current_cat_user.admin
-    #   raise "error"
-    # end
-    # ...
+    cat = Cat.find(params[:id])
+    cat.destroy
+    redirect_to cats_url
+
+    # GET /cats
+    # Click delete button
+    # Sends POST to /cats/123; but the _method = "DELETE" so rails
+    # understands it's a destroy
+    # destroy the cat
+    # issue redirect to /cats
+    # client gets /cats again
   end
 end
