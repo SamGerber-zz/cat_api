1687f62493620beda7933c61b9965cade9403d95
SamGerber <gerbyxvi@gmail.com>
2016-01-27 22:45:49 -0800

    Prepend withe cats table annotation

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime
+#  updated_at :datetime
+#  skill      :string
+#
+
 class Cat < ActiveRecord::Base
   validates :name, :skill, presence: true
   has_many :toys
